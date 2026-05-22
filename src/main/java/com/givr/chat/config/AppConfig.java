b398b16c609af82a7b6fc473724860ce33ef2276
jaden-2 <jedidiahamonia1@gmail.com>
2026-05-22 11:25:04 +0100

    removed allowed origins

--- src/main/java/com/givr/chat/config/AppConfig.java
+++ src/main/java/com/givr/chat/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import tools.jackson.databind.ObjectMapper;
+
 
 @Configuration
 @EnableWebFluxSecurity
--- src/main/java/com/givr/chat/config/WebSocketConfig.java
+++ src/main/java/com/givr/chat/config/WebSocketConfig.java
@@ -25,8 +25,6 @@ import java.util.Map;
 public class WebSocketConfig implements WebFluxConfigurer {
     @Autowired
     private JwtUtil jwtUtil;
-    @Value("${allowed.origins}")
-    private List<String> allowedOrigins;
 
     @Autowired
     private RedisService redisService;
